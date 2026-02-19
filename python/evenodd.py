e93ec40831f078edd1a7d862b3fc99b08443ef45
patelmukund7 <patelmukund661@gmail.com>
2026-02-19 21:46:25 +0530

    Add even-odd checker program

---
+++ python/evenodd.py
@@ -0,0 +1,7 @@
+# Program to check if a number is even or odd
+num = int(input("Enter a number: "))#input from user
+
+if num % 2 == 0:#even numbers are divisible by 2
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
